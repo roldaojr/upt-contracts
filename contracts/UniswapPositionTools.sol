f7f5d397df9b2ba1068393a6df96b855d633e714
Roldão Rego Jr <roldaogjr@gmail.com>
2022-11-30 19:20:48 -0300

    Enabled close position TWAP protection

--- contracts/UniswapPositionTools.sol
+++ contracts/UniswapPositionTools.sol
@@ -25,8 +25,6 @@ contract UniswapPositionTools is IUniswapPositionTools, ReentrancyGuard, Ownable
     uint128 constant Q64 = 2**64;
     uint128 constant Q96 = 2**96;
 
-    // max reward
-    uint64 constant public MAX_REWARD_X64 = uint64(Q64 / 50); // 2%
     // changable config values
     uint32 public override maxTWAPTickDifference = 100; // 1%
     uint32 public override TWAPSeconds = 60;
@@ -58,7 +56,7 @@ contract UniswapPositionTools is IUniswapPositionTools, ReentrancyGuard, Ownable
         int24 tickUpper;
     }
 
-    function _onlyTokenOwner(uint256 tokenId) view internal {
+    function _tokenOwnerCheck(uint256 tokenId) view internal {
         require(
             nonfungiblePositionManager.ownerOf(tokenId) == msg.sender,
             'Only token owner can change'
@@ -70,7 +68,7 @@ contract UniswapPositionTools is IUniswapPositionTools, ReentrancyGuard, Ownable
     ) override external nonReentrant returns (
         uint128 liquidity, uint256 compounded0, uint256 compounded1
     ) {
-        _onlyTokenOwner(tokenId);
+        _tokenOwnerCheck(tokenId);
         CompoundState memory state;
         // collect fees
         (state.amount0, state.amount1) = nonfungiblePositionManager.collect(
@@ -131,7 +129,7 @@ contract UniswapPositionTools is IUniswapPositionTools, ReentrancyGuard, Ownable
     ) override external nonReentrant returns (
         uint256 newTokenId, uint128 newLiquidity, uint256 amount0, uint256 amount1
     ) {
-        _onlyTokenOwner(tokenId);
+        _tokenOwnerCheck(tokenId);
         CompoundState memory state;
         // get position info
         uint128 liquidity;
@@ -197,7 +195,7 @@ contract UniswapPositionTools is IUniswapPositionTools, ReentrancyGuard, Ownable
     ) override external nonReentrant returns (
         uint256 amount0, uint256 amount1
     ) {
-        _onlyTokenOwner(tokenId);
+        _tokenOwnerCheck(tokenId);
         // get postion info
         (
             ,,address token0, address token1, uint24 fee,,,uint128 liquidity,,,,
@@ -225,28 +223,27 @@ contract UniswapPositionTools is IUniswapPositionTools, ReentrancyGuard, Ownable
         );
 
         // swap to requested token
-        if (conversion == RewardConversion.TOKEN_0) {
+        if (conversion == RewardConversion.TOKEN_0 && amount1 > 0) {
             // swap token1 to token0
             SafeERC20.safeIncreaseAllowance(
                 IERC20(token1), address(swapRouter), amount1
             );
-            amount0 += swapRouter.exactInput(
-                ISwapRouter.ExactInputParams(
-                    abi.encodePacked(token1, fee, token0),
-                    address(this), block.timestamp, amount1, 0
-                )
+            _getPrice(token0, token1, fee);
+            amount0 += _swap(
+                abi.encodePacked(token1, fee, token0),
+                amount1, block.timestamp
             );
             amount1 = 0;
-        } else if(conversion == RewardConversion.TOKEN_1) {
+        }
+        if(conversion == RewardConversion.TOKEN_1 && amount0 > 0) {
             // swap token0 to token1
             SafeERC20.safeIncreaseAllowance(
                 IERC20(token0), address(swapRouter), amount0
             );
-            amount1 += swapRouter.exactInput(
-                ISwapRouter.ExactInputParams(
-                    abi.encodePacked(token0, fee, token1),
-                    address(this), block.timestamp, amount0, 0
-                )
+            _getPrice(token0, token1, fee);
+            amount1 += _swap(
+                abi.encodePacked(token0, fee, token1),
+                amount0, block.timestamp
             );
             amount0 = 0;
         }
@@ -257,8 +254,22 @@ contract UniswapPositionTools is IUniswapPositionTools, ReentrancyGuard, Ownable
         emit LiquidityRemoved(msg.sender, tokenId, amount0, amount1, token0, token1);
     }
 
+    function _getPrice(address token0, address token1, uint24 fee) view internal returns (uint160 sqrtPriceX96) {
+        int24 tick;
+        IUniswapV3Pool pool = IUniswapV3Pool(factory.getPool(token0, token1, fee));   
+        (sqrtPriceX96, tick,,,,,) = pool.slot0();
+        // how many seconds are needed for TWAP protection
+        uint32 tSecs = TWAPSeconds;
+        if (tSecs > 0) {
+            // check that price is not too far from TWAP (protect from price manipulation attacks)
+            (int24 otherTick, bool twapOk) = _getTWAPTick(pool, tSecs);
+            require(twapOk, "No TWAP price");
+            _requireMaxTickDifference(tick, otherTick, maxTWAPTickDifference);
+        }
+    }
+
     //
-    // adapted from https://github.com/revert-finance/compoundor/blob/main/contracts/Compoundor.sol
+    // code adapted from https://github.com/revert-finance/compoundor/blob/main/contracts/Compoundor.sol
     //
     /**
      * @notice Management method to change the max tick difference from twap to allow swaps (onlyOwner)
@@ -336,71 +347,52 @@ contract UniswapPositionTools is IUniswapPositionTools, ReentrancyGuard, Ownable
         );
 
         // get price
-        IUniswapV3Pool pool = IUniswapV3Pool(factory.getPool(params.token0, params.token1, params.fee));
-
-        (state.sqrtPriceX96,state.tick,,,,,) = pool.slot0();
-
-        // how many seconds are needed for TWAP protection
-        uint32 tSecs = TWAPSeconds;
-        if (tSecs > 0) {
-            // check that price is not too far from TWAP (protect from price manipulation attacks)
-            (state.otherTick, state.twapOk) = _getTWAPTick(pool, tSecs);
-            if (state.twapOk) {
-                _requireMaxTickDifference(state.tick, state.otherTick, maxTWAPTickDifference);
-            } else {
-                // if there is no valid TWAP - disable swap
-                params.doSwap = false;
-            }
-        }
-
+        state.sqrtPriceX96 = _getPrice(params.token0, params.token1, params.fee);
         priceX96 = uint256(state.sqrtPriceX96).mul(state.sqrtPriceX96).div(Q96);
 
         // swap to correct proportions is requested
-        if (params.doSwap) {
+        // calculate ideal position amounts
+        state.sqrtPriceX96Lower = TickMath.getSqrtRatioAtTick(params.tickLower);
+        state.sqrtPriceX96Upper = TickMath.getSqrtRatioAtTick(params.tickUpper);
+        (state.positionAmount0, state.positionAmount1) = LiquidityAmounts.getAmountsForLiquidity(
+                                                            state.sqrtPriceX96,
+                                                            state.sqrtPriceX96Lower,
+                                                            state.sqrtPriceX96Upper,
+                                                            Q96); // dummy value we just need ratio
 
-            // calculate ideal position amounts
-            state.sqrtPriceX96Lower = TickMath.getSqrtRatioAtTick(params.tickLower);
-            state.sqrtPriceX96Upper = TickMath.getSqrtRatioAtTick(params.tickUpper);
-            (state.positionAmount0, state.positionAmount1) = LiquidityAmounts.getAmountsForLiquidity(
-                                                                state.sqrtPriceX96,
-                                                                state.sqrtPriceX96Lower,
-                                                                state.sqrtPriceX96Upper,
-                                                                Q96); // dummy value we just need ratio
-
-            // calculate how much of the position needs to be converted to the other token
-            if (state.positionAmount0 == 0) {
-                state.delta0 = amount0;
-                state.sell0 = true;
-            } else if (state.positionAmount1 == 0) {
-                state.delta0 = amount1.mul(Q96).div(priceX96);
-                state.sell0 = false;
+        // calculate how much of the position needs to be converted to the other token
+        if (state.positionAmount0 == 0) {
+            state.delta0 = amount0;
+            state.sell0 = true;
+        } else if (state.positionAmount1 == 0) {
+            state.delta0 = amount1.mul(Q96).div(priceX96);
+            state.sell0 = false;
+        } else {
+            state.amountRatioX96 = state.positionAmount0.mul(Q96).div(state.positionAmount1);
+            state.sell0 = (state.amountRatioX96.mul(amount1) < amount0.mul(Q96));
+            if (state.sell0) {
+                state.delta0 = amount0.mul(Q96).sub(state.amountRatioX96.mul(amount1)).div(state.amountRatioX96.mul(priceX96).div(Q96).add(Q96));
             } else {
-                state.amountRatioX96 = state.positionAmount0.mul(Q96).div(state.positionAmount1);
-                state.sell0 = (state.amountRatioX96.mul(amount1) < amount0.mul(Q96));
-                if (state.sell0) {
-                    state.delta0 = amount0.mul(Q96).sub(state.amountRatioX96.mul(amount1)).div(state.amountRatioX96.mul(priceX96).div(Q96).add(Q96));
-                } else {
-                    state.delta0 = state.amountRatioX96.mul(amount1).sub(amount0.mul(Q96)).div(state.amountRatioX96.mul(priceX96).div(Q96).add(Q96));
-                }
+                state.delta0 = state.amountRatioX96.mul(amount1).sub(amount0.mul(Q96)).div(state.amountRatioX96.mul(priceX96).div(Q96).add(Q96));
             }
+        }
 
-            if (state.delta0 > 0) {
-                if (state.sell0) {
-                    uint256 amountOut = _swap(
-                        abi.encodePacked(params.token0, params.fee, params.token1),
-                        state.delta0,
-                        params.deadline
-                    );
-                    amount0 = amount0.sub(state.delta0);
-                    amount1 = amount1.add(amountOut);
-                } else {
-                    state.delta1 = state.delta0.mul(priceX96).div(Q96);
-                    // prevent possible rounding to 0 issue
-                    if (state.delta1 > 0) {
-                        uint256 amountOut = _swap(abi.encodePacked(params.token1, params.fee, params.token0), state.delta1, params.deadline);
-                        amount0 = amount0.add(amountOut);
-                        amount1 = amount1.sub(state.delta1);
-                    }
+        if (state.delta0 > 0) {
+            if (state.sell0) {
+                uint256 amountOut = _swap(
+                    abi.encodePacked(params.token0, params.fee, params.token1),
+                    state.delta0,
+                    params.deadline
+                );
+                amount0 = amount0.sub(state.delta0);
+                amount1 = amount1.add(amountOut);
+            } else {
+                state.delta1 = state.delta0.mul(priceX96).div(Q96);
+                // prevent possible rounding to 0 issue
+                if (state.delta1 > 0) {
+                    uint256 amountOut = _swap(abi.encodePacked(params.token1, params.fee, params.token0), state.delta1, params.deadline);
+                    amount0 = amount0.add(amountOut);
+                    amount1 = amount1.sub(state.delta1);
                 }
             }
         }

f5fb1c02a87d2bb00797f846be646529caf17a46
Roldão Rego Jr <roldaogjr@gmail.com>
2023-01-28 16:15:39 -0300

    Updated findPositions

--- scripts/findPositions.js
+++ scripts/findPositions.js
@@ -1,4 +1,7 @@
+const { ethers } = require('hardhat')
 const uniswap = require("../lib/uniswap")
+
+const MAX_UINT128 = ethers.BigNumber.from(2).pow(128).sub(1)
 
 const main = async () => {
     console.info(`Search valid positions`)
@@ -8,17 +11,26 @@ const main = async () => {
     const totalPositions = await nfPositionManager.totalSupply()
     while(positions.length < positionsCount) {
         const tokenId = await nfPositionManager.tokenByIndex(
-            randomInteger(0, totalPositions.toNumber())
+            randomInteger(20000, totalPositions.toNumber())
         )
         const position = await nfPositionManager.positions(tokenId)
-        // check position balances
-        if(position.liquidity.gt(0) && (
-            position.tokensOwed0.gt(0) ||
-            position.tokensOwed1.gt(0)
-        )) {
-            console.info("Found valid position ID: ", tokenId.toNumber())
-            positions.push(tokenId.toNumber())
-        }
+        // check liquidity
+        if(position.liquidity.eq(0)) continue
+        const owner = await nfPositionManager.ownerOf(tokenId)
+        const signer = await ethers.getSigner(owner)
+        // check fees
+        const { amount0, amount1 } = await nfPositionManager.connect(
+            signer
+        ).callStatic.collect({
+            tokenId,
+            recipient: signer.address,
+            amount0Max: MAX_UINT128,
+            amount1Max: MAX_UINT128
+        }, {from: signer.address})
+        if(amount0.eq(0) && amount1.eq(0)) continue
+        // Position is valid
+        console.info("Found valid position ID:", tokenId.toNumber())
+        positions.push(tokenId.toNumber())
     }
     console.info("Valid positions found", positions)
 }
